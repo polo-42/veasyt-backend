b42911a14e8a73ce3f4fe47f05c97e3cb2615f12
Antonio.Dantas <antonio.dantas@etu.unige.ch>
2023-04-10 19:31:37 +0200

    feat: addresses_postal POST endpoint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from endpoints.rooms import rooms_endpoint
 from endpoints.catalogue import catalogue_endpoint
 from endpoints.inventory import inventory_endpoint
+from endpoints.addresses_postal import addressesPostal_endpoint
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -11,6 +12,7 @@ print(app.config['DB_USER'])
 app.register_blueprint(rooms_endpoint,url_prefix='/rooms')
 app.register_blueprint(catalogue_endpoint,url_prefix='/catalogue')
 app.register_blueprint(inventory_endpoint,url_prefix='/inventory')
+app.register_blueprint(addressesPostal_endpoint,url_prefix='/addressesPostal')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
